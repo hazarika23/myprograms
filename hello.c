71ec1e17116200854eae3cf9de4041591768f00b
bikash.hazarika@gmail.com <Bikash hazarika>
2020-06-20 05:02:34 -0400

    simple kernel thread introduced
    Signed-off-by: root <root@localhost.localdomain>

--- hello.c
+++ hello.c
@@ -4,17 +4,36 @@
 
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/kthread.h>
+
+static struct task_struct *mythreads=NULL;
+
+static int mythread_func(void *unused) {
+
+	printk(KERN_INFO"mythread_func is called\n");
+	return 0;
+}
 
 int init_module(void)
 {
-	printk(KERN_INFO "Hello world module inilization\n");
 
-	/*  A non 0 return means init_module failed; module can't be loaded. 
-	 */
+	printk(KERN_INFO"Creating my own thread\n");
+
+	mythreads=kthread_run(mythread_func,NULL,"mythread");
+	if (mythreads) {
+		printk(KERN_INFO "Thread is created sucssfully\n");
+	}
+	else {
+		printk(KERN_INFO "Unable to create a thread\n");
+	}
 	return 0;
 }
 
 void cleanup_module(void)
 {
+	if (mythreads) {
+		kthread_stop(mythreads);
+		printk(KERN_INFO "my thread is stop\n");
+	}
 	printk(KERN_INFO "Hello world module exit\n");
 }
