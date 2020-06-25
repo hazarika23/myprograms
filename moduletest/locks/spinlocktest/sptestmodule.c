13597b23ee6326eee79cfa88ecf40dec4e985d56
bikash.hazarika@gmail.com <Bikash Hazarika>
2020-06-25 12:07:42 -0400

    spinlock test
    Signed-off-by: root <root@localhost.localdomain>

---
+++ moduletest/locks/spinlocktest/sptestmodule.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/spinlock.h>
+
+DEFINE_SPINLOCK(mysplock1);
+spinlock_t mysplock2;
+
+int init_module(void)
+{
+	printk(KERN_INFO "Hello world\n");
+
+	spin_lock_init(&mysplock2);
+	spin_lock(&mysplock1);
+	spin_unlock(&mysplock1);
+	spin_lock(&mysplock2);
+	spin_unlock(&mysplock2);
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world\n");
+}
+
